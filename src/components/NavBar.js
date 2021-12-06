532650cd81f8c2009054fad3c6768a4ff2f3c925
Emmanuel-Develops <itakpeemma@gmail.com>
2021-12-06 20:10:35 +0100

    added roboto font to tailwind

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+
+const NavBar = () => {
+    return (
+        <nav className="">
+            <ul>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
