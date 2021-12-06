90e99ad37b44b1873be1b843f701dbb5867fba6d
Emmanuel-Develops <itakpeemma@gmail.com>
2021-12-06 22:20:02 +0100

    navbar mobile dropdown complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from "./components/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Aigis</header>
-    </div>
+    <Router>   
+      <div className="App">
+        <header className="App-header">
+          Aigis
+          <NavBar />
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,56 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowDropdownCircle } from "react-icons/io"
+
 
 
 const NavBar = () => {
-    return (
-        <nav className="">
-            <ul>
 
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const handleExpand = () => {
+        setIsExpanded((prev) => !prev)
+    }
+
+    return (
+        <nav className={`relative font-roboto max-w-6xl w-100 font-bold py-3 px-10 mx-auto border-t border-b border-gray-300 ${isExpanded? 'expand' : ""}`}>
+            <IoIosArrowDropdownCircle onClick={handleExpand} className={`dropdown ${isExpanded? "rotate" : ""}`}/>
+            <ul className="nav-items capitalize flex flex-wrap gap-x-10 gap-y-3 md:justify-between max-w-6xl mx-auto">
+                <li>
+                    <Link to={'/overview'}>
+                        overview
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/payments'}>
+                        payments
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/order'}>
+                        order
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/tracking'}>
+                        tracking
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/verification'}>
+                        verification
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/pricing'}>
+                        pricing
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/testimonials'}>
+                        testimonials
+                    </Link>
+                </li>
             </ul>
         </nav>
     )
