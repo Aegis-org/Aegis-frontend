94a9ad07987e4cbd31b11ec7620d50c9feb68801
Ayoola Oladipe <haywise20@gmail.com>
2021-12-07 11:13:29 +0100

    added hr bgcolor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        hrcolor: "#C4C4C4",
+      },
+    },
   },
   variants: {
     extend: {},
