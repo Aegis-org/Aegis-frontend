08ff35518056aa123f94e8e6005d35fbd5af756b
Emmanuel-Develops <itakpeemma@gmail.com>
2021-12-11 00:21:02 +0100

    useGetScreenBreakPoint updated

--- src/hooks/useGetScreenBreakPoint.jsx
+++ src/hooks/useGetScreenBreakPoint.jsx
@@ -2,25 +2,38 @@ import { useState, useEffect } from 'react'
 
 
 const useGetScreenBreakPoint = () => {
+
+    const initialState = getScreenSize(window.innerHeight)
     const [screen, setScreen] = useState(initialState)
 
+    function getScreenSize(current) {
+        if (current <= 640) {
+            return "sm"
+        } else if (current <= 768) {
+            return "md"
+        } else if (current <= 1024) {
+            return "lg"
+        } else if (current <= 1280) {
+            return "xl"
+        } else if (current > 1280) {
+            return "2xl"
+        }
+    }
+
     useEffect(() => {
-        const initial = window.innerHeight
-
-        function getScreenSize(current) {
-            let screen = ""
-            if (current <= 640) {
-                screen = "sm"
-            }
+        function updateScreen() {
+            const newScreen = getScreenSize(window.innerWidth)
+            setScreen(newScreen)
         }
-        window.addEventListener("resize", getScreenSize)
-        currentScreen
+        
+        window.addEventListener("resize", updateScreen)
+        
         return () => {
-            cleanup
+            window.removeEventListener("resize")
         }
-    }, [input])
+    }, [])
 
-    return {screen}
+    return screen
 }
 
 
