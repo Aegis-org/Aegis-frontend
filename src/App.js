1fd748d8085eb0000673f2654d2fd4d49c647007
Emmanuel-Develops <itakpeemma@gmail.com>
2021-12-13 12:33:09 +0100

    difficulty in using multiple headers at app.js, reverted to implementing headers in dynamic header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar";
 import Footer from "./footer/footer";
 import Home from "./pages/Home/Home";
 import DynamicHeader from "./components/DynamicHeader";
 
-
-
 function App() {
+  
   return (
     <Router>
       <div className="App">
-        {/* <Header>Aigis</Header>
-        <NavBar /> */}
-        <DynamicHeader />
+        <DynamicHeader/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
